32c23470430a82da7a0fe1afedcb0468dac52e72
raeufroushangar <roushnroushan19@gmail.com>
2018-05-25 01:56:54 -0400

    Add code to concatenate GEO platform files
    Python code for user to concatenate GEP platform files into one pandas dataframe and write into CSV file.

---
+++ ConcatenGEOPlatformFiles.py
@@ -0,0 +1,19 @@
+
+# Dependencies
+import os
+import pandas as pd
+
+# GEO platform files name must have "platform" and same directory
+os.chdir('set your directory here where GEO platform files located')
+
+platform_dataframes = []
+# Loop over platform files in directory
+for file in os.listdir():
+    if "platform" in file:
+        platform_file = pd.read_csv(file, sep="\t") # read file
+        platform_dataframes.append(platform_file)   # append file
+
+# Concatenate all dataframes
+result = pd.concat(platform_dataframes)
+# Write data into text file
+result.to_csv("platform_reference.txt", sep="\t",index=False, encoding='utf-8')
