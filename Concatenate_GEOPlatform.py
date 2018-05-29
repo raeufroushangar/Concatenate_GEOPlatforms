6f4c14bcaa7f707fbcc59c4d8e3c312d54778c0d
raeufroushangar <roushnroushan19@gmail.com>
2018-05-29 16:18:12 -0400

    Cleaned code: create Concatenate_GEOPlatform function
    Code been placed inside a function to clean code.

--- Concatenate_GEOPlatform.py
+++ Concatenate_GEOPlatform.py
@@ -3,18 +3,30 @@
 import os
 import pandas as pd
 
-# GEO platform files name must have "platform" and same directory
-os.chdir('set your directory here where GEO platform files located')
+# Concatenate GEO platform files into one main tab delimited text file:
+# function inputs:
+#-----------------
+#1st input: str() -- name of file that will be created
+#2nd input: str() -- directory path where GEO platform files are located
+#-----------------
+def Concatenate_GEOPlatform(file_name, GEOPlatform_files_directory):
+    
+    # GEO platform files name must have "platform" and same directory
+    os.chdir(GEOPlatform_files_directory)
+    
+    platform_dataframes = []
+    # Loop through platform files in directory
+    for file in os.listdir():
+        if "platform" in file:
+            platform_file = pd.read_csv(file, sep="\t") # read file
+            platform_dataframes.append(platform_file)   # append file
+    
+    # Concatenate all dataframes
+    result = pd.concat(platform_dataframes)
+    # Write data into text file
+    result.to_csv(file_name, sep="\t",index=False, encoding='utf-8')
 
-platform_dataframes = []
-# Loop through platform files in directory
-for file in os.listdir():
-    if "platform" in file:
-        platform_file = pd.read_csv(file, sep="\t") # read file
-        platform_dataframes.append(platform_file)   # append file
 
-# Concatenate all dataframes
-result = pd.concat(platform_dataframes)
-# Write data into text file
-result.to_csv("platform_reference.txt", sep="\t",index=False, encoding='utf-8')
-
+ 
+Concatenate_GEOPlatform("platform_reference.txt", 
+                        'set your directory here where GEO platform files located')
